efca91d1038d148dd80e5c7b5a30bb0152540ba1
HoracioEvGlez25 <153455858+HoracioEvGlez25@users.noreply.github.com>
2024-10-10 14:35:42 -0600

    Horacio

--- src/App.js
+++ src/App.js
@@ -92,7 +92,7 @@ function App() {
               <p className="mb-1">© 2024 Spin Records. Todos los derechos reservados 2024.</p>
               <p>Síguenos en nuestras redes sociales:</p>
               <a href="#" className="text-light me-3">Facebook.</a>
-              <a href="#" className="text-light me-3">Twitter</a>
+              <a href="#" className="text-light me-3">Twitter.</a>
               <a href="##########" className="text-light">Instagram</a>
             </div>
           </div>
